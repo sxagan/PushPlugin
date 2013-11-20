fa0a2124b30575d231491cbaa9a0b355eace18ce
Petrus Repo <petrus.repo@iki.fi>
2013-11-20 16:03:26 +0200

    Support multiple WebViews

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -10,31 +10,28 @@
 #import "PushPlugin.h"
 #import <objc/runtime.h>
 
+#import "ApplicationManager.h"
+
 static char launchNotificationKey;
 
-@implementation AppDelegate (notification)
-
-- (id) getCommandInstance:(NSString*)className
-{
-	return [self.viewController getCommandInstance:className];
-}
+@implementation AppGyverAppDelegate (notification)
 
 // its dangerous to override a method from within a category.
 // Instead we will use method swizzling. we set this up in the load call.
 + (void)load
 {
     Method original, swizzled;
-    
+
     original = class_getInstanceMethod(self, @selector(init));
     swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
     method_exchangeImplementations(original, swizzled);
 }
 
-- (AppDelegate *)swizzled_init
+- (AppGyverAppDelegate *)swizzled_init
 {
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createNotificationChecker:)
                name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
-	
+
 	// This actually calls the original init method over in AppDelegate. Equivilent to calling super
 	// on an overrided method, this is not recursive, although it appears that way. neat huh?
 	return [self swizzled_init];
@@ -53,29 +50,41 @@ static char launchNotificationKey;
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
-    [pushHandler didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+    ApplicationViewController *root = (ApplicationViewController*)[[ApplicationManager instance] currentRootViewController];
+
+    for (WebViewController *controller in [root.allWebViewControllers allValues]) {
+        PushPlugin *pushHandler = [controller getCommandInstance:@"PushPlugin"];
+        [pushHandler didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+    }
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
-    [pushHandler didFailToRegisterForRemoteNotificationsWithError:error];
+    ApplicationViewController *root = (ApplicationViewController*)[[ApplicationManager instance] currentRootViewController];
+
+    for (WebViewController *controller in [root.allWebViewControllers allValues]) {
+        PushPlugin *pushHandler = [controller getCommandInstance:@"PushPlugin"];
+        [pushHandler didFailToRegisterForRemoteNotificationsWithError:error];
+    }
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     NSLog(@"didReceiveNotification");
-    
+
     // Get application state for iOS4.x+ devices, otherwise assume active
     UIApplicationState appState = UIApplicationStateActive;
     if ([application respondsToSelector:@selector(applicationState)]) {
         appState = application.applicationState;
     }
-    
+
     if (appState == UIApplicationStateActive) {
-        PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
-        pushHandler.notificationMessage = userInfo;
-        pushHandler.isInline = YES;
-        [pushHandler notificationReceived];
+        ApplicationViewController *root = (ApplicationViewController*)[[ApplicationManager instance] currentRootViewController];
+
+        for (WebViewController *controller in [root.allWebViewControllers allValues]) {
+            PushPlugin *pushHandler = [controller getCommandInstance:@"PushPlugin"];
+            pushHandler.notificationMessage = userInfo;
+            pushHandler.isInline = YES;
+            [pushHandler notificationReceived];
+        }
     } else {
         //save it for later
         self.launchNotification = userInfo;
@@ -83,18 +92,23 @@ static char launchNotificationKey;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    
+
     NSLog(@"active");
-    
+
     //zero badge
     application.applicationIconBadgeNumber = 0;
 
-    if (![self.viewController.webView isLoading] && self.launchNotification) {
-        PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
-		
-        pushHandler.notificationMessage = self.launchNotification;
-        self.launchNotification = nil;
-        [pushHandler performSelectorOnMainThread:@selector(notificationReceived) withObject:pushHandler waitUntilDone:NO];
+    ApplicationViewController *root = (ApplicationViewController*)[[ApplicationManager instance] currentRootViewController];
+
+    for (WebViewController *controller in [root.allWebViewControllers allValues]) {
+
+        if (![controller.webView isLoading] && self.launchNotification) {
+            PushPlugin *pushHandler = [controller getCommandInstance:@"PushPlugin"];
+
+            pushHandler.notificationMessage = self.launchNotification;
+            self.launchNotification = nil;
+            [pushHandler performSelectorOnMainThread:@selector(notificationReceived) withObject:pushHandler waitUntilDone:NO];
+        }
     }
 }
 
