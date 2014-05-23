5bbb96277421de4f92d4596c7ae1a67c4954a4d9
asiolosy <asiolosy@sharklasers.com>
2014-05-23 09:05:52 +0200

    Update AppDelegate+notification.h

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -6,6 +6,8 @@
 //
 //
 
+#import "AppDelegate.h"
+
 @interface AppDelegate (notification)
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
